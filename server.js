2259e805b4f0ed7ab31e7f5f1ea353718b5d747d
jacobk44 <jacobkwartengamoah@gmail.com>
2026-03-29 05:15:48 -0700

    fix my server again

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const passport = require("passport");
 const session = require("express-session");
 const GitHubStrategy = require("passport-github2").Strategy;
-const cors = require('cors');
+const cors = require("cors");
 
 const port = process.env.PORT || 8080;
 
@@ -14,58 +14,73 @@ const port = process.env.PORT || 8080;
 
 app
   .use(bodyParser.json())
-  .use(session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-  }))
+  .use(
+    session({
+      secret: "secret",
+      resave: false,
+      saveUninitialized: true,
+    }),
+  )
   // This is the basic express sessions ({..}) initialization.
   .use(passport.initialize())
-  // init passport on every route call 
+  // init passport on every route call
   .use(passport.session())
-  // allow passport to use 'express-session' 
+  // allow passport to use 'express-session'
   .use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTION",
-  );
-  next();
-})
-.use(cors({origin: '*', method: ['GET', 'POST', 'PUT', 'DELETE','PATCH',]}))
-// Routes AFTER middleware
-.use("/", require("./routes/index.js"));
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTION",
+    );
+    next();
+  })
+  .use(cors({ origin: "*", method: ["GET", "POST", "PUT", "DELETE", "PATCH"] }))
+  // Routes AFTER middleware
+  .use("/", require("./routes/index.js"));
 
-passport.use(new GitHubStrategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: process.env.CALLBACK_URL
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    },
+  ),
+);
 
-},
-function(accessToken, refreshToken, profile, done){
-  return done(null, profile)
-}
-))
-
-passport.serializeUser((user, done) =>{
+passport.serializeUser((user, done) => {
   done(null, user);
-})
+});
 
-passport.deserializeUser((user, done) =>{
+passport.deserializeUser((user, done) => {
   done(null, user);
-})
+});
 
-app.get('/', (req,res) =>{res.send(req.session.user !== undefined ? `Legend is an ${req.session.user.displayName}`: "Logged Out")})
-app.get('/github/callback', passport.authenticate('github',{
-  failureRedirect: '/api-docs', session: false}),
-  (req,res) =>{
+app.get("/", (req, res) => {
+  res.send(
+    req.session.user !== undefined
+      ? `Legend in an ${req.session.user.displayName}`
+      : "Logged Out",
+  );
+});
+app.get(
+  "/github/callback",
+  passport.authenticate("github", {
+    failureRedirect: "/api-docs",
+    session: false,
+  }),
+  (req, res) => {
     req.session.user = req.user;
-    res.redirect('/');
-  });
+    res.redirect("/");
+  },
+);
 
 mongodb.initDb((err) => {
   if (err) {
